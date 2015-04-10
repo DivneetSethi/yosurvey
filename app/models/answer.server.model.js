d7bcd24fe21c210c3264a4ef6798f8b890b74a6e
Divneet Sethi <divneet91@yahoo.com>
2015-04-10 00:43:20 -0400

    answers module updated and fixed

---
+++ app/models/answer.server.model.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Answer Schema
+ */
+var AnswerSchema = new Schema({
+	name: {
+		type: String,
+		default: '',
+		required: 'Please fill Answer name',
+		trim: true
+	},
+    answer1: {
+		type: String,
+		default: '',
+		required: 'Please fill Answer 1',
+		trim: true
+	},
+    surveyid: {
+		type: String,
+		default: '',
+		required: 'Please fill id',
+		trim: true
+	},
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	user: {
+		type: Schema.ObjectId,
+		ref: 'User'
+	},
+    Survey: {
+		type: Schema.ObjectId,
+		ref: 'Survey'
+	}
+});
+
+mongoose.model('Answer', AnswerSchema);
